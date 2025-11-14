5d56e0ba91952559f59bf37aafa43a1ec74fead9
Ryan Souza <ryanssebadelhe@gmail.com>
2025-11-14 15:21:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/example/projectSM/services/PostService.java
@@ -0,0 +1,28 @@
+package com.example.projectSM.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.projectSM.domain.Post;
+import com.example.projectSM.repository.PostRepository;
+import com.example.projectSM.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+
+}
